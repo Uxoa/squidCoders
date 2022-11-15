050e9a75d266224085d30958038da686871dca30
FlorTiscornia <flora.tiscornia@gmail.com>
2022-11-15 06:01:17 -0300

    function JS to  turn doll and traffic ligth

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+   function cambiar () {
+    document.getElementById('matrix').src = "./images/lightRed.png";
+    document.getElementById('doll').src = "./images/dollFront.png";
+   }
+   
+   function volver () {
+    document.getElementById('matrix').src = "./images/lightGreen.png";
+    document.getElementById('doll').src = "./images/dollBack.png";
+   }
+   
