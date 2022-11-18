11519246a11b1aaa908012632373005a6e3989a3
Uxoa <palomita@gmail.com>
2022-11-18 09:11:01 +0100

    First basic game version

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
+function timeTrafficLight(name){ 
+   console.log(`Hola, mi nombre es ${name}`);
+   } 
+   setTimeout(timeTrafficLight, 1300, "Darío");
+
+
+
+const add = function(a,b){
+   console.log(a+b)
+ };
+ 
+ setTimeout(add,1000,1,2);
+ 
+ // 3 (in a second)
+
+
+function cambiar() {
+   document.getElementById("trafficLight").src="../images/lightRed.png";
+   document.getElementById("doll").src="../images/dollFront.png";
+};
 
 
 let squidersPlayer = ["Himorell", "Paloma", "Flor", "Leidy", "Sandra"];
 let removedSquiders = [];
+
 
 function toChooseSquider() {
     let numberPlayers = squidersPlayer.length; //Se inicializa con el tamaño del array//
@@ -15,29 +36,26 @@ function toChooseSquider() {
     console.log("Lista de vivos " + squidersPlayer); 
     console.log("Lista de squiders KO " + removedSquiders);
     return removedSquiders, selectedPlayer, squidersPlayer;
-   
 }
-
-
-const button = document.querySelector('button');
-
-button.onclick = function() {
-   toChooseSquider();
-  alert('Eliminados:' + removedSquiders);
-  cargaListadoVivos();
-  eliminados();
-};
-
-
-   function cargaListadoVivos(){
-
-      document.getElementById("vivos").innerHTML = (`${squidersPlayer.map((namePlayer) => `<span class="squidImage">${namePlayer}</span>`).join('')}`);
-};
-
-
-// dev
 
 function eliminados(){
    document.getElementById("eliminados").innerHTML = removedSquiders.join("<br/>");
-}
+};
 
+
+function cargaListadoVivos(){
+   document.getElementById("vivos").innerHTML = 
+   (`${squidersPlayer.map((namePlayer) => `<div class="squidImage">${namePlayer}</div>`).join('')}`);
+};
+
+const button = document.querySelector('button');
+   
+
+button.onclick = function() {
+   document.getElementById("vivos").innerHTML = (`${squidersPlayer.map((namePlayer) => `<span class="squidImage">${namePlayer}</span>`).join('')}`);
+   toChooseSquider()
+   cargaListadoVivos();
+   alert('Eliminados:' + removedSquiders);
+   eliminados();
+   cambiar();
+};
