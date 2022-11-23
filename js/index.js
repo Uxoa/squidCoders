e25e8516ec459842b3b754f54ef3c7304761246e
Uxoa <palomita@gmail.com>
2022-11-23 20:43:42 +0100

    Changing Button instrutions to home

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+let squidersPlayer = ["Himorell", "Paloma", "Flor", "Leidy", "Sandra"];
+let removedSquiders = [];
+let squiderLength = 0;
+function toChooseSquider() {
+   if ( squiderLength == squidersPlayer.length ) {
+      setTimeout(()=> Swal.fire({
+         title: 'GAME IS OVER',
+         iconHtml: '<img src="/images/overDoll.png" width="90" alt="Over Doll Icon"/>',
+         confirmButtonText: 'Play again!'
+   }).then((result) => {
+      if (result.isConfirmed) {
+         window.location.href = "index.html"
+      }}),);
+   }
+   if(squiderLength != squidersPlayer.length) {
+      let numberPlayers = squidersPlayer.length; //Se inicializa con el tamaño del array//
+      let index = Math.floor(Math.random() * numberPlayers);
+      let selectedPlayer = squidersPlayer[index];  //Guarda la variable con el valor de i//
+      console.log("squider eliminado es " + selectedPlayer);
+      removedSquiders.unshift(selectedPlayer);
+      squidersPlayer.splice(index,1);
+   }
+}
+
 function changePicture() {
    document.getElementById("trafficLight").src="images/lightRed.png";
    document.getElementById("doll").src="images/dollFront.png";
@@ -9,48 +33,15 @@ function changePictureBack() {
 };
 
 
-let squidersPlayer = ["Himorell", "Paloma", "Flor", "Leidy", "Sandra"];
-let removedSquiders = [];
-
-
-function toChooseSquider() {
-    let numberPlayers = squidersPlayer.length; //Se inicializa con el tamaño del array//
-    let index = Math.floor(Math.random() * numberPlayers);
-    let selectedPlayer = squidersPlayer[index];  //Guarda la variable con el valor de index//
-    console.log("squider eliminado es " + selectedPlayer);
-
-    removedSquiders.unshift(selectedPlayer); 
-    squidersPlayer.splice(index,1);
-  
-}
-
-
-
 function losers(){
    document.getElementById("losers").innerHTML = removedSquiders.join("<br/>");
    
 }
 
-
-
-
-
-
 function winners(){
    document.getElementById("winners").innerHTML = 
    (`${squidersPlayer.map((namePlayer) => `<span class="squidImage ` + this.getAttribute("data-id") + `">${namePlayer}</span>`).join('')}`);
 }
-
-
-
-
-
-/*const button = document.querySelector('button');
-button.onclick = addSquider() {
-   let nameNewSquider = document.getElementById("nameNewSquider").value;
-   nameNewSquider.unshift(squidersPlayer);
-
- }*/
 
  const button = document.querySelector('button');
    button.onclick = function() {
